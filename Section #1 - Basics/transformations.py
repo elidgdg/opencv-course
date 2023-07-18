0541ccf40cd7fc4caf20fbc0a74d9035c85169e3
Elijah Gatward <107950467+elidgdg@users.noreply.github.com>
2023-07-18 21:18:08 +0100

    translations

---
+++ Section #1 - Basics/transformations.py
@@ -0,0 +1,23 @@
+import cv2 as cv
+import numpy as np
+
+img = cv.imread('Resources/Photos/park.jpg')
+
+cv.imshow('Boston',img)
+
+# translation
+def translate(img, x, y):
+    transMat = np.float32([[1,0,x], [0,1,y]])
+    dimensions = (img.shape[1], img.shape[0])
+    return cv.warpAffine(img, transMat, dimensions)
+
+# -x: --> left
+# -y: --> up
+
+translated = translate(img, -100, 100)
+cv.imshow('translated', translated)
+
+# rotation
+
+
+cv.waitKey(0)
