355549446a0ad78b6351a5b35cc09a2b74a4dbd8
Elijah Gatward <107950467+elidgdg@users.noreply.github.com>
2023-07-19 20:00:18 +0100

    transformations

--- Section #1 - Basics/transformations.py
+++ Section #1 - Basics/transformations.py
@@ -18,6 +18,34 @@ translated = translate(img, -100, 100)
 cv.imshow('translated', translated)
 
 # rotation
+def rotate(img, angle, rotPoint=None):
+    (height, width) = img.shape[:2]
 
+    if rotPoint is None:
+        rotPoint = (width//2, height//2)
+
+    rotMat = cv.getRotationMatrix2D(rotPoint, angle, 1.0)
+    dimensions = (width, height)
+
+    return cv.warpAffine(img, rotMat, dimensions)
+
+rotated = rotate(img, -45)
+cv.imshow('Rotated', rotated)
+
+rotated_rotated = rotate(rotated, 90)
+cv.imshow('Rotated rotated', rotated_rotated)
+
+
+# resizing
+resized = cv.resize(img, (500,500), interpolation=cv.INTER_CUBIC)
+cv.imshow('resized', resized)
+
+# flip
+flip = cv.flip(img, -1)
+cv.imshow('flip', flip)
+
+# cropping
+cropped = img[200:400, 300:400]
+cv.imshow('cropped', cropped)
 
 cv.waitKey(0)
