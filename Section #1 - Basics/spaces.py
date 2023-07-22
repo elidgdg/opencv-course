36e756cd92242adae927fcdfcdc0e9248aa23bf3
Elijah Gatward <107950467+elidgdg@users.noreply.github.com>
2023-07-22 14:21:51 +0100

    color spaces

--- Section #1 - Basics/spaces.py
+++ Section #1 - Basics/spaces.py
@@ -16,6 +16,11 @@ lab = cv.cvtColor(img, cv.COLOR_BGR2LAB)
 cv.imshow('lab', lab)
  
 # BGR to RGB
+rgb = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+cv.imshow('rgb', rgb) # this comes out weird because opencv assumes it is bgr
 
+# HSV to BGR 
+hsv_bgr = cv.cvtColor(hsv, cv.COLOR_HSV2BGR)
+cv.imshow('Hsv-->BGR', hsv_bgr)
 
 cv.waitKey(0)
