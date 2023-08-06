5e7afbf3505d9e0f97ea4d5b7e5650db747583c7
Elijah Gatward <107950467+elidgdg@users.noreply.github.com>
2023-08-06 16:06:06 +0100

    face recognition

--- Section #3 - Faces/faces_train.py
+++ Section #3 - Faces/faces_train.py
@@ -8,5 +8,42 @@ for i in os.listdir('/home/elijah/repos/opencv-course/Resources/Faces/train'):
 
 DIR = '/home/elijah/repos/opencv-course/Resources/Faces/train'
 
+haar_cascade = cv.CascadeClassifier('/home/elijah/repos/opencv-course/Section #3 - Faces/haar_face.xml')
+
+features = []
+labels = []
+
 def create_train():
-    
+    for person in people:
+        path = os.path.join(DIR, person)
+        label = people.index(person)
+
+        for img in os.listdir(path):
+            img_path = os.path.join(path, img)
+
+            img_array = cv.imread(img_path)
+            gray = cv.cvtColor(img_array, cv.COLOR_BGR2GRAY)
+
+            faces_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=4)
+
+            for (x,y,w,h) in faces_rect:
+                faces_roi = gray[y:y+h, x:x+w]
+                features.append(faces_roi)
+                labels.append(label)
+
+create_train()
+print('Training done =============')
+
+
+features = np.array(features, dtype='object')
+labels = np.array(labels)
+
+
+face_recognizer = cv.face.LBPHFaceRecognizer_create()
+
+# train the recogniser on teh features list and the labels list
+face_recognizer.train(features, labels)
+
+face_recognizer.save('face_trained.yml')
+np.save('features.npy', features)
+np.save('labels.npy', labels)
